64383a0a4050d2b840ad477bca10ea204f482cd9
Vincent Quach <87631605+superlords1@users.noreply.github.com>
2022-12-31 18:04:36 -0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -9,58 +9,104 @@ import os
 from dotenv import load_dotenv
 import time
 
-def getPosts(num):
+def get_top(num):
     PATH = "C:\\Users\\vince\\chromedriver.exe"
     chrome_options = webdriver.ChromeOptions()
     prefs = {"profile.default_content_setting_values.notifications" : 2}
+    #chrome_options.add_argument('--headless')
     chrome_options.add_experimental_option("prefs",prefs)
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
     driver = webdriver.Chrome(service=Service(PATH), chrome_options=chrome_options)
     driver.get("https://www.facebook.com/login/device-based/regular/login/?login_attempt=1&next=https%3A%2F%2Fwww.facebook.com%2Fgroups%2F553266151399305")
-    
-    # log in
+    action = webdriver.ActionChains(driver)
+
     driver.implicitly_wait(1)
     load_dotenv()
     driver.find_element(By.ID, "email").send_keys(os.getenv('email'))
     driver.find_element(By.ID, "pass").send_keys(os.getenv('pass'))
     driver.find_element(By.ID, "loginbutton").click()
-    ListOfLinks = []
-    ListOfTexts = []
-    LinkCount, TextCount = 0, 0
-    
-    while LinkCount < num:
+
+    listOfLinks = []
+    listOfItems = []
+
+    while len(listOfLinks) < num:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         time.sleep(3)
         listings = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@CLASS = 'x1yztbdb x1n2onr6 xh8yej3 x1ja2u2z']")))
+
         for list in listings:
-            try:
-                element = list.find_element(By.XPATH, ".//li[contains(@class, 'x1rg5ohu x1emribx x1i64zmx')]/a")
-                comment_link = element.get_attribute('href')
-                split = comment_link.split('?comment')
-                link = split[0]
-            except:
-                pass
-            try:
-                if link not in ListOfLinks:
-                    ListOfLinks.append(link)
-                    ListOfTexts.append(list.text.split("Message")[0])
-                    LinkCount += 1
-                    if len(ListOfLinks) >= num:
-                        #return [ListOfLinks, ListOfTexts]
-                        break
-                else:
+            if len(listOfLinks) < num and len(listOfItems) < num:
+                try:
+                    second = list.find_element(By.XPATH, ".//span[contains(@CLASS, 'x4k7w5x x1h91t0o')]/a")
+                    action.move_to_element(second)
+                    action.perform()
+                    link = second.get_attribute('href').split('?')[0]
+                    if link not in listOfLinks:
+                        listOfLinks.append(link)
+
+                    item = list.find_element(By.XPATH, ".//span[contains(@CLASS, 'x1lliihq x6ikm8r x10wlt62 x1n2onr6 x1j85h84')]/div").text
+                    if item not in listOfItems:
+                        listOfItems.append(item)  
+                except:
                     pass
-            except:
-                pass
-    '''       
-    for i in range(LinkCount):
-        print(ListOfLinks[i])
-        print(ListOfTexts[i])
-        print("----------------------------------------------------")
-    '''
-    return ListOfLinks, ListOfTexts
+            else:
+                break
+
+    return listOfLinks, listOfItems
+
+
+def get_recent(num):
+    PATH = "C:\\Users\\vince\\chromedriver.exe"
+    chrome_options = webdriver.ChromeOptions()
+    prefs = {"profile.default_content_setting_values.notifications" : 2}
+    #chrome_options.add_argument('--headless')
+    chrome_options.add_experimental_option("prefs",prefs)
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(service=Service(PATH), chrome_options=chrome_options)
+    driver.get("https://www.facebook.com/login/device-based/regular/login/?login_attempt=1&next=https%3A%2F%2Fwww.facebook.com%2Fgroups%2F553266151399305%3Fsorting_setting%3DCHRONOLOGICAL_LISTINGS")
+    action = webdriver.ActionChains(driver)
+    driver.implicitly_wait(1)
+
     
-        
+
+    load_dotenv()
+    driver.find_element(By.ID, "email").send_keys(os.getenv('email'))
+    driver.find_element(By.ID, "pass").send_keys(os.getenv('pass'))
+    driver.find_element(By.ID, "loginbutton").click()
+
+    listOfLinks = []
+    listOfItems = []
+
+    while len(listOfLinks) < num:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(3)
+        listings = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//div[@CLASS = 'x1yztbdb x1n2onr6 xh8yej3 x1ja2u2z']")))
+
+        for list in listings:
+            if len(listOfLinks) < num and len(listOfItems) < num:
+                try:
+                    second = list.find_element(By.XPATH, ".//span[contains(@CLASS, 'x4k7w5x x1h91t0o')]/a")
+                    action.move_to_element(second)
+                    action.perform()
+                    link = second.get_attribute('href').split('?')[0]
+                    if link not in listOfLinks:
+                        listOfLinks.append(link)
+
+                    item = list.find_element(By.XPATH, ".//span[contains(@CLASS, 'x1lliihq x6ikm8r x10wlt62 x1n2onr6 x1j85h84')]/div").text
+                    if item not in listOfItems:
+                        listOfItems.append(item)  
+                except:
+                    pass
+            else:
+                break
+
+    return listOfLinks, listOfItems
+                
+    
+         
+           
+
+
         
 
 
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-from bot import getPosts
+from bot import get_top
+from bot import get_recent
 import os
 from dotenv import load_dotenv
 
@@ -11,18 +12,30 @@ bot.remove_command("help")
 @bot.event
 async def on_ready():
     print("We have logged in as {0.user}".format(bot))
+    #get_recent(1)
 
 @bot.command(name = "rodney")
-async def stats(ctx, args):
-    links, texts = getPosts(int(args))
-    embed = discord.Embed(title = 'POSTS')
+async def stats(ctx, args, args2):
+    if args2 == 'recent':
+        links, texts = get_recent(int(args))
+    elif args2 == 'top':
+        links, texts = get_top(int(args))
+    else:
+        await ctx.send(embed = discord.Embed(title = "Please specify 'top' or 'recent'"))
+        return
+
+    embed = discord.Embed(title = f"{args2} Listings")
     for i in range(int(args)):
         embed.add_field(name = links[i], value = texts[i], inline = True)
     await ctx.send(embed = embed)
 
 @bot.command(name = "help")
 async def help(ctx):
-    await ctx.send(embed = discord.Embed(title = "STANCED CAR PARTS", description = "To get parts: /rodney <amount of listings>"))
+    await ctx.send(embed = discord.Embed(title = "STANCED CAR PARTS", description = "To get parts: /rodney <amount of listings> <top/recent>"))
 
+@bot.command(name = "dabmeup")
+async def dabmeup(ctx):
+    await ctx.send(file=discord.File('dabmeup.gif'))
+    
 load_dotenv()
 bot.run(os.getenv('token'))
